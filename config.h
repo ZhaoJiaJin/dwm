59a7f9b3ef359cad1b23a47f72e717cc1d308ffb
ZhaoJiaJin <zhaojin0905@gmail.com>
2020-06-13 22:03:18 +0100

    change bar pos

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Monaco for Powerline:size=10","FontAwesome:size=11","Wuncon Siji:size=11" };
 static const char dmenufont[]       = "Monaco for Powerline:size=10";
 
